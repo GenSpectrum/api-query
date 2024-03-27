f01cec5265fdc2c1368262bbee07af212d95792c
Christian Jaeger <ch@christianjaeger.ch>
2024-03-27 16:38:55 +0000

    Works for siloApi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Response;
+use tokio;
+use anyhow::{Result, Context, anyhow};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let query = std::env::var("QUERY").with_context(|| anyhow!("need QUERY env var"))?;
+    let client = reqwest::Client::new();
+    let res: Response = client.post("http://localhost:8081/query")
+        .body(query)
+        .send()
+        .await?;
+    // https://docs.rs/reqwest/0.12.2/reqwest/struct.Response.html
+    let s = res.text().await?;
+    println!("{s}");
+    Ok(())
 }
