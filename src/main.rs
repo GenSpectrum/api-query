f2f9ba50d9b9bd6a4313a0c5a8837147bbae39e5
Christian Jaeger <ch@christianjaeger.ch>
2024-05-10 14:11:08 +0200

    Use locked stdout
    Buffering should not really be an issue as it bypasses copying into
    the buffer after 1024 bytes.[1]
    
    [1] https://users.rust-lang.org/t/efficient-stdout-its-buffers-all-the-way-down/66669

--- src/main.rs
+++ src/main.rs
@@ -56,17 +56,19 @@ async fn main() -> Result<()> {
         .send()
         .await
         .with_context(|| anyhow!("posting the query {query:?}"))?;
+    let mut out = stdout().lock();
     // https://docs.rs/reqwest/0.12.2/reqwest/struct.Response.html
     while let Some(bytes) = res.chunk().await
         .with_context(|| anyhow!("reading the result from query {query:?}"))?
     {
-        stdout().write_all(&bytes)
+        out.write_all(&bytes)
             .with_context(|| anyhow!("writing to stdout"))?;
     }
     if res.status() != 200 {
-        stdout().write_all(b"\n")
+        out.write_all(b"\n")
             .with_context(|| anyhow!("writing to stdout"))?;
         std::process::exit(1);
     }
+    out.flush()?;
     Ok(())
 }
